36ba1874095fcca975bc09438677baacb3fc71f7
Shaun <nhlamulosmashaba@gmail.com>
2024-12-09 14:17:48 +0200

    fixed images url

--- venuerental/settings.py
+++ venuerental/settings.py
@@ -48,8 +48,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'https://venueimages.netlify.app'
+MEDIA_ROOT = None
 # Application definition
 
 INSTALLED_APPS = [
